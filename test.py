c6eb91c3fd00947de318f81b18b2447ae2acabfb
Byron McDowell <bgroot2021@gmail.com>
2026-02-27 19:04:25 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+import os
+
+def main():
+    # Point directly to your test file or folder
+    project_path = "C:/Users/bgroo/PycharmProjects/PlaywrightUITests/automated-ui-sorting-check.py"
+    print(f"Running tests in: {project_path}\n")
+
+    result = subprocess.run(
+        [sys.executable, "-m", "pytest", project_path, "-s"],
+        cwd=os.getcwd(),
+        stdout=sys.stdout,
+        stderr=sys.stderr
+    )
+
+    if result.returncode == 0:
+        print("\n✅ All tests passed.")
+    else:
+        print("\n❌ Some tests failed.")
+        sys.exit(result.returncode)
+
+if __name__ == "__main__":
+    main()
